fed2b7901f97bd22e65f44dbf1b59b2b844d3c5f
Nakul Mishra <nakulmishra746@gmail.com>
2024-02-15 16:58:35 +0530

    fixed try catch statement

--- src/atm/Register.java
+++ src/atm/Register.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Register {
     static Scanner sc = new Scanner(System.in);
     public void registration(){
-        System.out.println("Welcome to registration page...");
-        System.out.print("Enter your name: ");String name = sc.nextLine();
-        System.out.print("Enter your 9 digit account number: ");int account_number = sc.nextInt();
-        System.out.print("Enter your pin: ");int pin = sc.nextInt();
         try{
+            System.out.println("Welcome to registration page...");
+            System.out.print("Enter your name: ");String name = sc.nextLine();
+            System.out.print("Enter your 9 digit account number: ");int account_number = sc.nextInt();
+            System.out.print("Enter your pin: ");int pin = sc.nextInt();
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm?characterEncoding=utf8","root","root18");
             String sql = "insert into atm.users"+"(account_number, pin, name)"+" values (?,?,?)";
